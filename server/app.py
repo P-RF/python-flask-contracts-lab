637abd9708d86fcca7a8707fcd7ff438e835ff48
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2025-09-19 10:34:55 -0400

    Added contract and customer routes

--- server/app.py
+++ server/app.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
 
-# Imports the Flask class (and others) from the flask library.
 from flask import Flask, request, current_app, g, make_response
 
 contracts = [{"id": 1, "contract_information": "This contract is for John and building a shed"},{"id": 2, "contract_information": "This contract is for a deck for a buisiness"},{"id": 3, "contract_information": "This contract is to confirm ownership of this car"}]
+
 customers = ["bob","bill","john","sarah"]
 
-# Creates an instance of the Flask application. __name__ tells Flask where to look for resources. appis the object used to configure routes and run the server.
 app = Flask(__name__)
 
-# The route decorator tells Flask: when someone visits this URL in the browser, run the function below.
 @app.route('/contract/<int:id>')
-def contract(id):    # defines a function named contract. Takes an argument (id) that Flask automatically passesfrom the route.
-    return f'Contract ID: {id}'   # Returns a string response to the client
+def contract(id):
+    for contract in contracts:
+        if contract['id'] == id:
+            return contract['contract_information'], 200
+    return '404: Contract not found', 404
 
-# The route decorator matches URLs with the name of a customer. Whatever comes after /customer/ is passed to the function as customer_name.
 @app.route('/customer/<customer_name>')
-def customer(customer_name):    # Defines a funciton for the /customer/ route. customer_name holds whatever is passed in the URL, e.g., Bob.
-    return f'Customer Name: {customer_name}'    # Returns a string with the customer name.
+def customer(customer_name):
+    if customer_name.lower() in [customer.lower() for customer in customers]:
+        return '', 204
+    else:
+        return '404: Customer not found', 404
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
