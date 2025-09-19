958e1713069d26511226ec3acebd6fd61dfb150b
Priscila Rodriguez <priscila.rodriguezfresco@gmail.com>
2025-09-19 11:29:07 -0400

    completed lab

--- server/app.py
+++ server/app.py
@@ -8,19 +8,19 @@ customers = ["bob","bill","john","sarah"]
 
 app = Flask(__name__)
 
-@app.route('/contract/<int:id>')
-def contract(id):
-    for contract in contracts:
-        if contract['id'] == id:
-            return contract["contract_information"], 200
-    return '404: Contract not found', 404
+@app.route('/contract/<int:id>') # Route decorator is used to handle a request for a dynamic URL. 
+def contract(id): 
+    for contract in contracts:  # For loop iterates over the contracts list.
+        if contract['id'] == id:    # Checks if the contract's id matches the id from the URL.
+            return contract["contract_information"], 200    # Returns a response to the client.
+    return '404: Contract not found', 404   # Handles the case if no contract is found.
 
-@app.route('/customer/<customer_name>')
+@app.route('/customer/<customer_name>') # Route decorator is used to handle a request to URLs that match /customer/<customer_name>. 
 def customer(customer_name):
-    if customer_name.lower() in [customer.lower() for customer in customers]:
-        return '', 204
+    if customer_name.lower() in [customer.lower() for customer in customers]:   # Checks if the provided name exists in the customers list (case-sensitive).
+        return '', 204  # Runs an empty response with status code 204.
     else:
-        return '404: Customer not found', 404
+        return '404: Customer not found', 404   # Handles the case where a customer is not found.
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
